fcb94de4fec88ff382a0d8abf2ab2acf53c71c8b
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-06-13 20:50:14 +0100

    First capacitor test build done ( saved android folder )

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.mrzscanner',
   appName: 'mrz-scan',
   webDir: 'www'
 };
