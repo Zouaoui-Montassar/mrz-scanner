6d31a2e790c4d9319a7e3e7fcfca46c1f45a497b
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-06-16 19:43:54 +0100

    Camera test improvement for the home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
@@ -8,15 +8,29 @@ import { Camera, CameraResultType } from '@capacitor/camera';
 })
 export class HomePage {
   capturedImage: string | undefined;
+  imageLoadError: boolean = false;
 
   constructor() {}
 
   async takePicture() {
-    const image = await Camera.getPhoto({
-      quality: 90,
-      allowEditing: false,
-      resultType: CameraResultType.DataUrl
-    });
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl
+      });
 
-    this.capturedImage = image.dataUrl;
-}}
+      this.capturedImage = image.dataUrl;
+      this.imageLoadError = false; 
+    } catch (error) {
+      console.error('Error capturing image', error);
+      this.capturedImage = undefined;
+      this.imageLoadError = true; 
+    }
+  }
+
+  onImageError() {
+    this.capturedImage = undefined;
+    this.imageLoadError = true;
+  }
+}
