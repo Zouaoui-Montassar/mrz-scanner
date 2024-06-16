c0642cd85ef2fd36df34f17d9ed459e2533879f2
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-06-16 13:52:52 +0100

    Capacitor camera test + tesseract ocr library

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
-export class HomePage implements OnInit {
+export class HomePage {
+  capturedImage: string | undefined;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl
+    });
 
-}
+    this.capturedImage = image.dataUrl;
+}}
