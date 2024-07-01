caf315ade3f47bb7e9de6ba243e822b957c97ddb
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-07-01 13:00:56 +0100

    Http plugin , added text error

--- frontend/src/app/stored-codes/stored-codes.page.ts
+++ frontend/src/app/stored-codes/stored-codes.page.ts
@@ -8,6 +8,9 @@ import { PassportService } from '../services/passport.service';
 })
 export class StoredCodesPage {
   passports: any[] = [];
+  loading = false;
+  error = false;
+  errorText = "";
 
   constructor(private passportService: PassportService) {}
 
@@ -16,11 +19,25 @@ export class StoredCodesPage {
   }
 
   loadPassports() {
-    this.passportService.getPassports().subscribe((res:any) => {
-      console.log("data: ",res);
-      this.passports=res.data.data;
-    })
+    this.loading = true;
+    this.error = false;
+    this.passportService.getPassports().subscribe((res: any) => {
+      console.log("Response from backend:", res);
+      if (res && res.data && res.data.data) {
+        this.passports = res.data.data;
+        console.log("Passports loaded:", this.passports);
+      } else {
+        console.error("Invalid response format:", res);
+        this.error = true;
+      }
+    }, error => {
+      console.error("Error loading passports:", error);
+      this.error = true;
+      this.loading = false;
+      this.errorText=error;
+    });
   }
+  
   
   
 }
